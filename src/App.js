623e484797e045080f67c561805d190ffcd9d594
Carlos Hernandez <chernandezmiranda@gmail.com>
2024-01-13 15:05:00 -0800

    Non working app. dependencies issues.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,59 @@
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux'
 import { useEffect } from 'react';
+import { fetchData, prevImage, nextImage, setArtId, reset } from './features/dataSlice';
 
-function App(props) {
+const mapStateToProps = state => state.data
+
+let debounce = (func, wait) => {
+  let timeout;
+  return (...args) => {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args)
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  }
+}
+
+function App({artId}) {
   const dispatch = useDispatch()
   const data = useSelector((state) => state.data)
 
   const renderImg = () => {
-    if(data?.apiData) {
+    if(data?.apiData?.primaryImage) {
       return <img style={{'width': '100vw'}} src={data.apiData.primaryImage} alt={data.apiData.title} />
     } else {
       return <p>image here</p>
     }
   }
 
+  const onInput = debounce(e => {
+    dispatch(setArtId(e.target.value))
+  }, 500)
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [artId]);
+
   return (
     <div className="App">
       <div>
         <button onClick={() => {
-          // dispatch fetchData
+          dispatch (fetchData())
         }}>Thunk!</button>
         <button onClick={() => {
-          // dispatch reset
+          dispatch (reset())
         }}>Clear</button>
         <button onClick={() => {
-          // dispatch next
+          dispatch (nextImage())
         }}>Next</button>
         <button onClick={() => {
-          // dispatch prev
+          dispatch (prevImage())
         }}>Back</button>
       </div>
-      <input value={ data?.artId } onChange={(e) => {
-        // dispatch setArtId
-      }} />
+      <input value={ data?.artId } onChange={onInput} />
       <div>
         {data?.artId}
         {renderImg()}
@@ -41,4 +62,4 @@ function App(props) {
   );
 }
 
-export default App;
+export default connect(mapStateToProps)App;
